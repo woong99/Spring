6d815f4d0958afa695aee202fa94159232b2da0d
woong99 <dndry1072@naver.com>
2022-07-06 21:39:46 +0900

    response

---
+++ response/src/main/java/com/example/response/controller/ApiController.java
@@ -0,0 +1,29 @@
+package com.example.response.controller;
+
+import com.example.response.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    // TEXT
+    @GetMapping("/text")
+    public String text(@RequestParam String account) {
+        return account;
+    }
+
+    // JSON
+    @PostMapping("/json")
+    public User json(@RequestBody User user){
+        return user;
+    }
+
+    // ResponseEntity
+    @PutMapping("/put")
+    public ResponseEntity<User> put(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+}
