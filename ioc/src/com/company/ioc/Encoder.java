b74ac831dc68d289854d26f42714284e0bc1c4d6
woong99 <dndry1072@naver.com>
2022-07-06 22:40:31 +0900

    ioc

---
+++ ioc/src/com/company/ioc/Encoder.java
@@ -0,0 +1,15 @@
+package com.company.ioc;
+
+import java.util.Base64;
+
+public class Encoder implements IEncoder {
+
+    private IEncoder iEncoder;
+
+    public Encoder(IEncoder iEncoder){
+        this.iEncoder = iEncoder;
+    }
+    public String encode(String message) {
+        return iEncoder.encode(message);
+    }
+}
