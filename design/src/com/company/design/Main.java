a7dd9cf7e4b93aa9e78989b447d38eb5b48acae9
woong99 <dndry1072@naver.com>
2022-07-06 16:35:48 +0900

    Fasade-Design

--- design/src/com/company/design/Main.java
+++ design/src/com/company/design/Main.java
@@ -1,8 +1,10 @@
 package com.company.design;
 
 import com.company.design.adapter.Electronic110V;
-import com.company.design.observer.Button;
-import com.company.design.observer.IButtonListener;
+import com.company.design.facade.Ftp;
+import com.company.design.facade.Reader;
+import com.company.design.facade.SftpClient;
+import com.company.design.facade.Writer;
 
 public class Main {
     public static void main(String[] args) {
@@ -76,17 +78,41 @@ public class Main {
 //        audi5.showPrice();
 
 //        ** Observer Design Example **
-        Button button = new Button("버튼");
-        button.addListener(new IButtonListener() {
-            @Override
-            public void clickEvent(String event) {
-                System.out.println(event);
-            }
-        });
-        button.click("메시지 전달 : Click 1");
-        button.click("메시지 전달 : Click 2");
-        button.click("메시지 전달 : Click 3");
-        button.click("메시지 전달 : Click 4");
+//        Button button = new Button("버튼");
+//        button.addListener(new IButtonListener() {
+//            @Override
+//            public void clickEvent(String event) {
+//                System.out.println(event);
+//            }
+//        });
+//        button.click("메시지 전달 : Click 1");
+//        button.click("메시지 전달 : Click 2");
+//        button.click("메시지 전달 : Click 3");
+//        button.click("메시지 전달 : Click 4");
+
+//        ** Facade Design Example **
+        Ftp ftpClient = new Ftp("www.foo.co.kr", 22, "/home/etc");
+        ftpClient.connect();
+        ftpClient.moveDirectory();
+
+        Writer writer = new Writer("text.tmp");
+        writer.fileConnect();
+        writer.write();
+
+        Reader reader = new Reader("text.tmp");
+        reader.fileConnect();
+        reader.fileRead();
+
+        reader.fileDisconnect();
+        writer.fileDisconnect();
+        ftpClient.disConnect();
+
+        SftpClient sftpClient = new SftpClient("www.foo.co.kr", 22, "/home/etc", "text.tmp");
+
+        sftpClient.connect();
+        sftpClient.write();
+        sftpClient.read();
+        sftpClient.disConnect();
 
     }
 
