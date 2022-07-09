19a3b1950c3399e8f36b5f5bc00c9ed1bcd44538
woong99 <dndry1072@naver.com>
2022-07-09 19:33:40 +0900

    client/

---
+++ client/src/main/java/com/example/client/dto/Req.java
@@ -0,0 +1,51 @@
+package com.example.client.dto;
+
+public class Req<T> {
+
+    private Header header;
+
+    private T responseBody;
+
+    public static class Header {
+        private String responseCode;
+
+        public String getResponseCode() {
+            return responseCode;
+        }
+
+        public void setResponseCode(String responseCode) {
+            this.responseCode = responseCode;
+        }
+
+        @Override
+        public String toString() {
+            return "Header{" +
+                    "responseCode='" + responseCode + '\'' +
+                    '}';
+        }
+    }
+
+    public Header getHeader() {
+        return header;
+    }
+
+    public void setHeader(Header header) {
+        this.header = header;
+    }
+
+    public T getResponseBody() {
+        return responseBody;
+    }
+
+    public void setResponseBody(T responseBody) {
+        this.responseBody = responseBody;
+    }
+
+    @Override
+    public String toString() {
+        return "Req{" +
+                "header=" + header +
+                ", body=" + responseBody +
+                '}';
+    }
+}
