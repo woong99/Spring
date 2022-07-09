b38c1eb6a1d44a230c85dee6c9e58f59671f1eef
woong99 <dndry1072@naver.com>
2022-07-09 20:02:59 +0900

    server/

--- server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -3,12 +3,50 @@ package com.example.server.controller;
 import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @RestController
 @Slf4j
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    @GetMapping("/naver")
+    public String naver() {
+        String query = "갈비집";
+        String encode = Base64.getEncoder().encodeToString(query.getBytes(StandardCharsets.UTF_8));
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("v1/search/local.json")
+                .queryParam("query", "갈비집")
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+
+        log.info(uri.toString());
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "lYh2pqBS5FBypfuNXWZ_")
+                .header("X-Naver-Client-Secret", "D07I0eVkBr")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+        return result.getBody();
+    }
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age) {
