f27c20a2a4b3f8f631d52e2495b3bdf640e34cf9
woong99 <dndry1072@naver.com>
2022-07-26 18:20:39 +0900

    ex03

---
+++ ex03/src/main/java/controller/SampleController.java
@@ -0,0 +1,68 @@
+package controller;
+
+import domain.SampleVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@RestController
+@RequestMapping("/sample")
+@Slf4j
+public class SampleController {
+
+    @GetMapping(value = "/getText", produces = "text/plain; charset=UTF-8")
+    public String getText() {
+        log.info("MIME TYPE: " + MediaType.TEXT_PLAIN_VALUE);
+
+        return "안녕하세요";
+    }
+
+    @GetMapping(value = "/getSample", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public SampleVO getSample() {
+        return new SampleVO(112, "스타", "로드");
+    }
+
+    @GetMapping("/getSample2")
+    public SampleVO getSample2() {
+        return new SampleVO(113, "로켓", "라쿤");
+    }
+
+    @GetMapping("/getList")
+    public List<SampleVO> getList() {
+        return IntStream.range(1, 10).mapToObj(i -> new SampleVO(i, i + "First", i + " Last"))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/getMap")
+    public Map<String, SampleVO> getMap() {
+        Map<String, SampleVO> map = new HashMap<>();
+        map.put("First", new SampleVO(111, "그루트", "주니어"));
+
+        return map;
+    }
+
+    @GetMapping(value = "/check", params = {"height", "weight"})
+    public ResponseEntity<SampleVO> check(Double height, Double weight) {
+        SampleVO vo = new SampleVO(0, "" + height, "" + weight);
+
+        ResponseEntity<SampleVO> result = null;
+
+        if (height < 150) {
+            result = ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(vo);
+        } else {
+            result = ResponseEntity.status(HttpStatus.OK).body(vo);
+        }
+
+        return result;
+    }
+}
---
+++ ex03/src/main/webapp/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: woong_9yo
+  Date: 2022/07/26
+  Time: 5:45 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
