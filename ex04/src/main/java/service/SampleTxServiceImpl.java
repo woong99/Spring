76aadfb2e66d38bb5f5ee9320d8a650a1b6b23bb
woong99 <dndry1072@naver.com>
2022-07-28 22:29:23 +0900

    ex04

---
+++ ex04/src/main/java/service/SampleTxServiceImpl.java
@@ -0,0 +1,31 @@
+package service;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import mapper.Sample1Mapper;
+import mapper.Sample2Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+public class SampleTxServiceImpl implements SampleTxService {
+
+    @Setter(onMethod_ = {@Autowired})
+    private Sample1Mapper mapper1;
+
+    @Setter(onMethod_ = {@Autowired})
+    private Sample2Mapper mapper2;
+
+    @Transactional
+    @Override
+    public void addData(String value) {
+        log.info("mapper1.........................");
+        mapper1.insertCol1(value);
+        log.info("mapper2.........................");
+        mapper2.insertCol2(value);
+
+        log.info("end.............................");
+    }
+}
