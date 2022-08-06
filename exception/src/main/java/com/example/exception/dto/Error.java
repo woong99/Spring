8f971d0abd3ababaae1c97d58e12c4414a62884f
woong99 <dndry1072@naver.com>
2022-08-06 19:50:22 +0900

    Study Exception

---
+++ exception/src/main/java/com/example/exception/dto/Error.java
@@ -0,0 +1,11 @@
+package com.example.exception.dto;
+
+import lombok.Data;
+
+@Data
+public class Error {
+
+    private String field;
+    private String message;
+    private String invalidValue;
+}
