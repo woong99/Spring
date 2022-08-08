4995d3fff53f521f59c44769f1080ab40ebc2baf
woong99 <dndry1072@naver.com>
2022-08-08 15:05:49 +0900

    server/

--- server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -4,7 +4,15 @@ import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/api/server")
@@ -35,5 +43,41 @@ public class ServerApiController {
         response.setResBody(user.getResBody());
 
         return response;
+    }
+
+
+    @GetMapping("/naver")
+    public String naver(){
+        // https://openapi.naver.com/v1/search/blog.json
+        // ?query=%EB%A6%AC%EB%B7%B0
+        // &display=10
+        // &start=1
+        // &sort=sim
+
+        String query = "갈비집";
+        String encode = Base64.getEncoder().encodeToString(query.getBytes(StandardCharsets.UTF_8));
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/blog.json")
+                .queryParam("query", query)
+                .queryParam("display" , 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "sim")
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "lYh2pqBS5FBypfuNXWZ_")
+                .header("X-Naver-Client-Secret", "D07I0eVkBr")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
     }
 }
