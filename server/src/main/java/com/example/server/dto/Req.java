bc87d30cbc93fa59831ea58b626e0655208f9625
woong99 <dndry1072@naver.com>
2022-08-08 14:57:01 +0900

    server/

--- server/src/main/java/com/example/server/dto/Req.java
+++ server/src/main/java/com/example/server/dto/Req.java
@@ -1,22 +1,17 @@
 package com.example.server.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Req<T> {
 
     private Header header;
+
     private T resBody;
 
     @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Header {
+    public static class Header{
         private String responseCode;
     }
 }
-
--- server/src/main/java/com/example/server/dto/User.java
+++ server/src/main/java/com/example/server/dto/User.java
@@ -1,12 +1,8 @@
 package com.example.server.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     private String name;
     private int age;
